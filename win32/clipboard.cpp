ccef6e3b8d25a611f1e728c35d0494796613226c
IG <aloneguid@outlook.com>
2022-11-07 13:23:20 +0000

    Herpderp, shoulda check if it does really compile.

--- win32/clipboard.cpp
+++ win32/clipboard.cpp
@@ -10,7 +10,7 @@ namespace win32 {
     // links:
     // - https://learn.microsoft.com/en-us/windows/win32/dataxchg/using-the-clipboard
 
-    void clipboard::set_text(const std::string& text) {
+    void clipboard::set_ascii_text(const std::string& text) {
         if (!::OpenClipboard(nullptr)) return;
         if (!::EmptyClipboard()) return;
         HGLOBAL gh = ::GlobalAlloc(GMEM_MOVEABLE, text.size() + 1);
@@ -25,7 +25,7 @@ namespace win32 {
         ::GlobalFree(gh);
     }
 
-    std::string clipboard::get_text() {
+    std::string clipboard::get_ascii_text() {
         if (!::IsClipboardFormatAvailable(CF_TEXT)) return "";
         if (!OpenClipboard(nullptr)) return "";
         string r;
@@ -33,6 +33,27 @@ namespace win32 {
         HGLOBAL gh = ::GetClipboardData(CF_TEXT);
         if (gh != NULL) {
             LPSTR lpstr = (LPSTR)::GlobalLock(gh);
+            if (lpstr != NULL) {
+
+                r = lpstr;
+
+                ::GlobalUnlock(lpstr);
+            }
+        }
+
+        ::CloseClipboard();
+
+        return r;
+    }
+
+    std::wstring clipboard::get_text() {
+        if (!::IsClipboardFormatAvailable(CF_UNICODETEXT)) return L"";
+        if (!OpenClipboard(nullptr)) return L"";
+        wstring r;
+
+        HGLOBAL gh = ::GetClipboardData(CF_UNICODETEXT);
+        if (gh != NULL) {
+            LPWSTR lpstr = (LPWSTR)::GlobalLock(gh);
             if (lpstr != NULL) {
 
                 r = lpstr;
--- win32/clipboard.h
+++ win32/clipboard.h
@@ -4,7 +4,8 @@
 namespace win32 {
     class clipboard {
     public:
-        static void set_text(const std::string& text);
-        static std::string get_text();
+        static void set_ascii_text(const std::string& text);
+        static std::string get_ascii_text();
+        static std::wstring get_text();
     };
 }
