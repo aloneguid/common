db4a3d038932858401c6ab0bacd454626f23c99f
IG <aloneguid@outlook.com>
2024-08-13 10:53:41 +0100

    added super-widget 2.0.

--- win32/popup_menu.cpp
+++ win32/popup_menu.cpp
@@ -4,6 +4,7 @@
 namespace win32 {
     popup_menu::popup_menu(HWND h_wnd_owner) 
         : h_wnd_owner{ h_wnd_owner } {
+
         clear();
     }
 
@@ -11,6 +12,34 @@ namespace win32 {
         clear();
         ::DestroyMenu(h_menu);
     }
+
+    /*HBITMAP IconToBitmap(HICON hIcon) {
+        HDC hDC = GetDC(NULL); // Get the screen's device context
+        HDC hMemDC = CreateCompatibleDC(hDC); // Create a memory device context compatible with the screen
+
+        ICONINFO iconInfo;
+        GetIconInfo(hIcon, &iconInfo); // Get information about the icon
+
+        BITMAP bm;
+        GetObject(iconInfo.hbmColor, sizeof(BITMAP), &bm); // Get information about the icon's bitmap
+
+        HBITMAP hBitmap = CreateCompatibleBitmap(hDC, bm.bmWidth, bm.bmHeight); // Create a bitmap compatible with the screen
+        HGDIOBJ hOldObj = SelectObject(hMemDC, hBitmap); // Select the new bitmap into the memory DC
+
+        // Draw the icon onto the bitmap
+        DrawIconEx(hMemDC, 0, 0, hIcon, bm.bmWidth, bm.bmHeight, 0, NULL, DI_NORMAL);
+
+        SelectObject(hMemDC, hOldObj); // Restore the old object
+        DeleteDC(hMemDC); // Delete the memory device context
+        ReleaseDC(NULL, hDC); // Release the screen's device context
+
+        // Clean up
+        DeleteObject(iconInfo.hbmColor);
+        DeleteObject(iconInfo.hbmMask);
+
+        return hBitmap; // Return the handle to the bitmap
+    }*/
+
 
     void popup_menu::add(const std::string& id, const std::string& title, bool disabled) {
         auto loword_wparam = next_id++;
@@ -22,6 +51,15 @@ namespace win32 {
             flags, 
             loword_wparam,
             str::to_wstr(title).c_str());
+
+        // note: the below works, but looks ugly
+        //HINSTANCE hi = ::GetModuleHandle(NULL);
+        //HICON hIcon = LoadIcon(NULL, IDI_ASTERISK);
+        //HBITMAP hb = ::LoadBitmap(hi, MAKEINTRESOURCE(IDC_ICON));
+        //HBITMAP hBitmap = IconToBitmap(hIcon);
+        //::SetMenuItemBitmaps(*h_menu_nesting.rbegin(),
+        //    loword_wparam,
+        //    MF_BYCOMMAND, hBitmap, nullptr);
 
         // note: to have icons, entire menu item needs to be owner drawn :(
 
